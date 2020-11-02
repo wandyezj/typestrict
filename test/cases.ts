d11324fef09442bb5eac34ab69d9aae0d2836238
wandyezj <wandyezj@gmail.com>
2020-11-02 16:00:42 -0600

    SyntaxKind

--- test/cases.ts
+++ test/cases.ts
@@ -1,28 +1,6 @@
+import * as casesJson  from "./examples.json"
 // should also enable rules to enable for the case
 
 // really want to discover the syntax kind set for each example
 // describe syntax allowable and then ban everything else
-export const cases: { name?: string, pass: boolean, code: string }[] = [
-    {
-        pass: true,
-        code: `const x = 0'`
-    },
-    {
-        pass: true,
-        code: `let x = "value"`,
-    },
-    {
-        pass: true,
-        code: `let x = "value"`,
-    },
-    {
-        pass: true,
-        code: `for (let i =0; i < 3; i++) {}`
-    },
-    {
-        pass: false,
-        code: `var x = "value"`,
-    }
-    ,
-
-]
+export const cases: { name?: string, pass: boolean, code: string }[] = casesJson;
--- test/manual.ts
+++ test/manual.ts
@@ -1,1 +1,11 @@
-console.log("test")
+import {cases, syntaxKinds} from "./external"
+
+console.log("test");
+
+cases.forEach((example) => {
+    const {code} = example;
+
+    const kinds = syntaxKinds(code);
+    console.log(code);
+    console.log(kinds.join("\n"))
+});
